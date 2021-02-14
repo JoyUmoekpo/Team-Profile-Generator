6b9ab0aa8b2d32c97b159cc0c6dd8ba528ee75bd
Joy <joyumoekpo1@gmail.com>
2021-02-13 21:24:02 -0600

    Added inquirer question function

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
+//Jest and Inquirer
 const jest = require("jest");
 const inquirer = require("inquirer");
 
+//Requiring other files
 const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
+
+//Inquirer Prompt Questions
+function questions(answers) {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "list",
+            name: "position",
+            message: "What is your position?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your ID number?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub profile name?"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "What is your office number?"
+        }
+    ]).then(function (res) {
+        console.log(res)
+    }
+    )
+}
+
+questions();
