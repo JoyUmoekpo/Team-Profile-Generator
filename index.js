d1859b767d23b5f9dbe933b06c61f182814f04b4
Joy <joyumoekpo1@gmail.com>
2021-02-13 23:44:22 -0600

    Added function for html

--- index.js
+++ index.js
@@ -70,6 +70,29 @@ function intialQuestions(answers) {
     });
 };
 
+function htmlFile() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile Page</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark"><span>Team Profile</span></nav>
 
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./output/members.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
 
+function startApp(){
 intialQuestions();
+htmlFile();
+};
+
+startApp();
